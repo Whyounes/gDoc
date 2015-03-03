1907a45e18e26bf27e494a8c34834f7f9405bb3e
RAFIE Younes <younes.rafie@gmail.com>
2015-03-03 15:32:35 +0000

    Updated composer

--- rafie.php
+++ rafie.php
@@ -1,13 +1,7 @@
 <?php
-include_once __DIR__ . '/../vendor/autoload.php';
 
 use RAFIE\Application;
 
 $app = new Application();
 
 $app->run();
-
-// Take the Git files from Sami and put them inside my repo.
-// Refactor code
-// Make some tests if you are a men :/
-// Create the theme stuff
